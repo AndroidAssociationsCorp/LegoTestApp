3ee7fab25cb49e640e70e573ba7fac518ae040be
Militia <dimitri20111@mail.ru>
2020-04-27 15:08:24 +0500

    A few new activities and some other stuff

--- app/src/main/java/com/example/legotestapp/MainActivity.java
+++ app/src/main/java/com/example/legotestapp/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.legotestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
     }
 }
