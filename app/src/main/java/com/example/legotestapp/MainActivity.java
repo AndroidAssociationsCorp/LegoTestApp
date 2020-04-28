df1de1157b9ff37348826d8db1a2cee056098f41
Militia <dimitri20111@mail.ru>
2020-04-28 12:51:28 +0500

    xxx

--- app/src/main/java/com/example/legotestapp/MainActivity.java
+++ app/src/main/java/com/example/legotestapp/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.legotestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
-
+ Button button = findViewById(R.id.button_1);
     public void onClick(View view) {
+        button.setBackgroundColor(Color.YELLOW);
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
         startActivity(intent);
     }
